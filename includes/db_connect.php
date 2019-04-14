8dd2e30140687055d426f6a677413b28d08232b5
cvexa <cvexxxxa@abv.bg>
2019-04-14 14:05:24 +0300

    extra file deleted

--- includes/db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn = mysqli_connect('localhost', 'cvexa', 'onemanarmy', 'goodness');
-
-if(!$conn){
-	die("Connection failed:" . mysqli_connect_error());
-} else {
-	// echo '<h1>Connected successfully!</h1>';
-}
